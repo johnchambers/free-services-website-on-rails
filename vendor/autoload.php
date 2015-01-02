18a84224cbf941c92695a97ae45de3c38242e8d9
John Chambers <johncham81@gmail.com>
2015-01-02 13:16:33 +0000

    gitignore updated

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitd2bc41358c43ee4779e759527cbc04d6::getLoader();
+return ComposerAutoloaderInit74c410eb3a4e1b68fa53a61ccc39dbb9::getLoader();
